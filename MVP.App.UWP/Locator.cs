5c62c970219c23fd03d259d3d7218c957cafecd7
James Croft <jamz_c@hotmail.co.uk>
2017-02-03 13:03:27 +0000

    Added structure to UWP app.

---
+++ MVP.App.UWP/Locator.cs
@@ -0,0 +1,42 @@
+﻿namespace MVP.App
+{
+    using GalaSoft.MvvmLight.Ioc;
+    using GalaSoft.MvvmLight.Messaging;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    using MVP.Api;
+    using MVP.App.ViewModels;
+
+    /// <summary>
+    /// Defines a locator for application view-models and services.
+    /// </summary>
+    public class Locator
+    {
+        public Locator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            RegisterServices();
+            RegisterViewModels();
+        }
+
+        public InitializingPageViewModel InitializingPageViewModel
+            => ServiceLocator.Current.GetInstance<InitializingPageViewModel>();
+
+        public MainPageViewModel MainPageViewModel => ServiceLocator.Current.GetInstance<MainPageViewModel>();
+
+        private static void RegisterServices()
+        {
+            SimpleIoc.Default.Register<IMessenger, Messenger>();
+
+            SimpleIoc.Default.Register<ApiClient>();
+        }
+
+        private static void RegisterViewModels()
+        {
+            SimpleIoc.Default.Register<InitializingPageViewModel>();
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+    }
+}
---
+++ MVP.App.UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace MVP.App.ViewModels
+{
+    using MVP.App.Views;
+
+    using Windows.UI.Xaml.Navigation;
+
+    using WinUX.MvvmLight.Xaml.Views;
+
+    /// <summary>
+    /// Defines the view-model for the <see cref="MainPage"/>
+    /// </summary>
+    public class MainPageViewModel : PageBaseViewModel
+    {
+        /// <inheritdoc />
+        public override void OnPageNavigatedTo(NavigationEventArgs args)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void OnPageNavigatedFrom(NavigationEventArgs args)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void OnPageNavigatingFrom(NavigatingCancelEventArgs args)
+        {
+        }
+    }
+}
