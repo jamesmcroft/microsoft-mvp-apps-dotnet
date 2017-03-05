09fe872af0c9a6baf3383a77805e4d2ea4d92b14
James Croft <jamz_c@hotmail.co.uk>
2017-03-05 10:54:15 +0000

    Added icons into UWP project and started to add category to icon converter.

---
+++ MVP.App.UWP/Converters/AwardCategoryToIconConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace MVP.App.Converters
+{
+    using System;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Data;
+
+    public class AwardCategoryToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
