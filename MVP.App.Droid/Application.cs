2c8a4580256ff2d3847d4957044ddaa6dd9e7baa
James Croft <jamz_c@hotmail.co.uk>
2018-12-11 16:09:35 +0000

    Cleared out project

--- MVP.App.Droid/Application.cs
+++
@@ -1,9 +0,0 @@
-namespace MVP.App
-{
-    public static class Application
-    {
-        private static Locator locator;
-
-        public static Locator Locator => locator ?? (locator = new Locator());
-    }
-}
